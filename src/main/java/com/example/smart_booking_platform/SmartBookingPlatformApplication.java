959a01ba5924cdf81296c5fb458d3bf884ee4b49
GaganiSenarathne <gagani@iphonik.com>
2026-02-19 12:13:06 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/example/smart_booking_platform/SmartBookingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_booking_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBookingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBookingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smart_booking_platform/SmartBookingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_booking_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBookingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
