910079afe590c469f26f9da72482a5b2ac6bab35
GaganiSenarathne <gagani@iphonik.com>
2026-05-11 12:02:02 +0530

    Resource and Booking Implementation

--- src/main/java/com/gagani/smart_booking_platform/entity/Booking.java
+++ src/main/java/com/gagani/smart_booking_platform/entity/Booking.java
@@ -17,11 +17,11 @@ public class Booking {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private UserInfo user;
 
     @ManyToOne
-    @JoinColumn(name = "id", nullable = false)
+    @JoinColumn(name = "resource_id", nullable = false)
     private Resource resource;
 
     private Instant startTime;
